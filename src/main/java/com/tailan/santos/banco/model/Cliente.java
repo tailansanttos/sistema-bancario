ce3a667d9a0f2efca966b90a15fc95c68069fc6d
Tailan <tailansanttos02@gmail.com>
2025-04-26 18:59:34 -0300

    feat: Entidade cliente criada.

---
+++ src/main/java/com/tailan/santos/banco/model/Cliente.java
@@ -0,0 +1,82 @@
+package com.tailan.santos.banco.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    @CPF(message = "Campo CPF precisa ser válido.")
+    private String cpf;
+
+    @Email(message = "Campo email tem que ser válido.")
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String cpf, String email, String telefone, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
