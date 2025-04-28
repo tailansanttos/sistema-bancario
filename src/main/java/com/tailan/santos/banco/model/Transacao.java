fbde9795bc4452cc2da337926fb85c3e70d126b0
Tailan <tailansanttos02@gmail.com>
2025-04-28 00:36:52 -0300

    feat: a

--- src/main/java/com/tailan/santos/banco/model/Transacao.java
+++ src/main/java/com/tailan/santos/banco/model/Transacao.java
@@ -27,4 +27,72 @@ public class Transacao {
 
     @Enumerated(EnumType.STRING)
     private TransacaoStatus status;
+
+    public Transacao() {
+    }
+
+    public Transacao(BigDecimal valor, LocalDateTime dataHora, TransacaoTipo tipo, Conta contaOrigem, Conta contaDestino, TransacaoStatus status) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+        this.tipo = tipo;
+        this.contaOrigem = contaOrigem;
+        this.contaDestino = contaDestino;
+        this.status = status;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public TransacaoTipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TransacaoTipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Conta getContaOrigem() {
+        return contaOrigem;
+    }
+
+    public void setContaOrigem(Conta contaOrigem) {
+        this.contaOrigem = contaOrigem;
+    }
+
+    public Conta getContaDestino() {
+        return contaDestino;
+    }
+
+    public void setContaDestino(Conta contaDestino) {
+        this.contaDestino = contaDestino;
+    }
+
+    public TransacaoStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransacaoStatus status) {
+        this.status = status;
+    }
 }
