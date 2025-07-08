5fd58018da9b2bd3aa6ec61179d7dc908d0ef6a5
tailan <tailansanttos02@gmail.com>
2025-07-08 11:25:39 -0300

    refactor: adicionado validações

--- src/main/java/com/tailan/santos/banco/dtos/cliente/ClienteRequestDto.java
+++ src/main/java/com/tailan/santos/banco/dtos/cliente/ClienteRequestDto.java
@@ -1,4 +1,19 @@
 package com.tailan.santos.banco.dtos.cliente;
 
-public record ClienteRequestDto(String nome, String cpf, String email, String telefone, String endereco) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record ClienteRequestDto(
+        @NotNull(message = "Nome não pode ser nulo.")
+        String nome,
+        @CPF(message = "Campo CPF precisa ser válido!")
+        String cpf,
+        @Email(message = "Campo email precisa ser válido.")
+        String email,
+        @NotNull(message = "Telefone não pode ser nulo.")
+        String telefone,
+        @NotNull(message = "Endereço não pode ser nulo.")
+        String endereco)
+{
 }
